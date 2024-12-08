f14f50ce449a44cafe331ad6a9b74bb5fb30b089
Chethan <chethanreddy0503@gmail.com>
2024-12-08 06:37:16 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
-const { nextui } = require("@nextui-org/react");
-const withMT = require("@material-tailwind/react/utils/withMT");
+import { nextui } from "@nextui-org/react";
+import withMT from "@material-tailwind/react/utils/withMT";
 
-/** @type {import('tailwindcss').Config} */
-module.exports = withMT({
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    // Add the backend directory if you have any Tailwind CSS files there
+    "./backend/**/*.{js,ts,jsx,tsx}", // Example path
   ],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'], // Custom font definition
+        roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui(),
-    require('daisyui'),
-  ]
+  plugins: [nextui(), require('daisyui')],
 });
